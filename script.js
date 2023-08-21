3480940757a6ce10c8fae1bbcf09f18fc5585e9e
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-08-21 13:02:56 +0200

    Added comments to JS

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ heading.classList.toggle('drkmode');
 const toggleFunc = () => {
   heading.classList.toggle('drkmode');
 };
+
+
+
+// const enableDMode = () => {
+  //turn on CSS
+  //set local storage to 'enabled'
+// };
+
+// const disableDMode = () => {
+  //turn off CSS
+  //set local storage to 'disabled'
+// };
+
+// const buttonToClick = () => {
+  //get local storage key
+  //check if key is 'enabled' or 'disabled'
+  //if key is 'enabled,' turn on CSS
+  // else if key is 'disabled', turn off CSS
+// };
+
+//run buttonToClick()  on page load in order to know what last checked option was ?

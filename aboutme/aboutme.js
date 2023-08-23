847d669e678de27d1d8b9f3f3bd25c0f2f72c212
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-08-23 15:14:33 +0200

    Added dark mode to all pages, ignored checkbox

--- aboutme/aboutme.js
+++ aboutme/aboutme.js
@@ -1,14 +1,37 @@
 const heading = document.getElementById('htmlMain');
 const image = document.getElementById('img01');
-
-heading.classList.toggle('drkmode');
-
-const toggleFunc = () => {
-  heading.classList.toggle('drkmode');
-};
+// const checkbox = document.getElementById('check-box');
 
 image.classList.toggle('imgclass');
 
-const toggleImg = () => {
-  image.classList.toggle('imgclass');
+const enableDMode = () => {
+  heading.classList.add('drkmode');
+  image.classList.add('imgclass');
+  window.localStorage.setItem('darkmode', 'enabled');
 };
+
+const disableDMode = () => {
+  heading.classList.remove('drkmode');
+  image.classList.remove('imgclass');
+  localStorage.setItem('darkmode', 'disabled');
+};
+
+const update = () => {
+  const key = localStorage.getItem('darkmode');
+  if (key === 'enabled') {
+    enableDMode();
+  } else {
+    disableDMode();
+  };
+};
+
+const buttonToClick = () => {
+  const key = localStorage.getItem('darkmode');
+  if (key === 'disabled') {
+    enableDMode();
+  } else {
+    disableDMode();
+  };
+};
+
+update();
--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,32 @@
 const heading = document.getElementById('htmlMain');
+// const checkbox = document.getElementById('check-box');
 
-heading.classList.toggle('drkmode');
-
-const toggleFunc = () => {
-  heading.classList.toggle('drkmode');
+const enableDMode = () => {
+  heading.classList.add('drkmode');
+  window.localStorage.setItem('darkmode', 'enabled');
 };
+
+const disableDMode = () => {
+  heading.classList.remove('drkmode');
+  localStorage.setItem('darkmode', 'disabled');
+};
+
+const update = () => {
+  const key = localStorage.getItem('darkmode');
+  if (key === 'enabled') {
+    enableDMode();
+  } else {
+    disableDMode();
+  };
+};
+
+const buttonToClick = () => {
+  const key = localStorage.getItem('darkmode');
+  if (key === 'disabled') {
+    enableDMode();
+  } else {
+    disableDMode();
+  };
+};
+
+update();

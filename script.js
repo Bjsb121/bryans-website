a5cc77bbd02448d64f157b2d280306449ee2fc1a
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-08-30 15:20:34 +0200

    Finished 3D model CSS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const heading = document.getElementById('htmlMain');
 const hamburgerBox = document.getElementById('hamburgerBox');
+const viewPort = document.getElementById('view-port');
+
+viewPort.classList.toggle('dark');
 
 const toggleButton = () => {
   hamburgerBox.classList.toggle('hamburger-box');
@@ -7,11 +10,13 @@ const toggleButton = () => {
 
 const enableDMode = () => {
   heading.classList.add('drkmode');
+  viewPort.classList.add('dark');
   window.localStorage.setItem('darkmode', 'enabled');
 };
 
 const disableDMode = () => {
   heading.classList.remove('drkmode');
+  viewPort.classList.remove('dark');
   localStorage.setItem('darkmode', 'disabled');
 };
 

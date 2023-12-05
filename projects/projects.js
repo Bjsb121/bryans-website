7e9e6e19e205b63f251bbd3b3239880a8373cc8f
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-12-06 00:02:46 +0100

    Changed white and images

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,8 @@
 const heading = document.getElementById('htmlMain');
+const image = document.getElementById('img01');
 const hamburgerBox = document.getElementById('hamburgerBox');
+
+image.classList.toggle('imgclass');
 
 const toggleButton = () => {
   hamburgerBox.classList.toggle('hamburger-box');
@@ -7,11 +10,13 @@ const toggleButton = () => {
 
 const enableDMode = () => {
   heading.classList.add('drkmode');
+  image.classList.add('imgclass');
   window.localStorage.setItem('darkmode', 'enabled');
 };
 
 const disableDMode = () => {
   heading.classList.remove('drkmode');
+  image.classList.remove('imgclass');
   localStorage.setItem('darkmode', 'disabled');
 };
 
